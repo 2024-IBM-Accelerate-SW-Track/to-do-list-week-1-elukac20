44c9a4247e7ec8133b1aadb49381d8d273b5d352
elukac20 <141880386+elukac20@users.noreply.github.com>
2024-06-07 17:30:35 -0400

    all tests pass

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
-  
+
+import "./About.css";
+import profile_pic from "../assets/profile_pic.jpg";
+
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p>
+          
+        <div>
+          <div className="split left">
+          <div className="centered">
+          <img 
+          className="profile_image"
+          src={profile_pic}
+          alt="Profile Pic"
+          ></img>
+          </div>
+          </div>
+          <div className="split right">
+          <div className="centered">
+          <div className="name_title">Elena Lukac</div>
+          <div className="brief_description">
+          I'm a junior at Barnard College studying Computer Science.
+          </div>
+          </div>
+          </div>
+          </div>
+
+</p> 
       </div>
     )
   }
