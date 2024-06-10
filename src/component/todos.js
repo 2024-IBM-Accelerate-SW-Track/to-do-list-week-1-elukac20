338065c782a8081c9dce6be391985b57053b7f48
elukac20 <141880386+elukac20@users.noreply.github.com>
2024-06-10 01:42:07 -0400

    works

--- src/component/todos.js
+++ src/component/todos.js
@@ -17,15 +17,20 @@ var globalList;
 
 /*added this function to delete tasks*/
 const clearTask = () =>{
-  for(var i=0; i<globalList.length; i++){
-    globalList.pop();
+  for(var i=globalList.length-1; i>=0; i--){
+    var itm = document.getElementById(globalList.pop().id);
+    itm.innerHTML="deleted"
   }
+  
+  
 }
 
 const deleteTask = (id) =>{
-  alert(id)
+  //alert(id)
   var indx = globalList.indexOf(document.getElementById(id))
   globalList.splice(indx, 1);
+  var itm = document.getElementById(id);
+  itm.innerHTML="deleted"
   
 }
 
@@ -46,8 +51,8 @@ const Todos = ({ todos }) => {
                 <span style={{ padding: "20px" }}>
                   <ListItemButton>
                     <Checkbox color="success"></Checkbox>
-                    
-                       <ListItemText>{todo.content}</ListItemText> 
+                        
+                       <ListItemText id={todo.id}>{todo.content}</ListItemText> 
                        <button onClick={function(){deleteTask(todo.id)}}>
                           <DeleteIconOutlined></DeleteIconOutlined>
                         </button>
@@ -56,7 +61,6 @@ const Todos = ({ todos }) => {
               </CardContent>
             </Card>
           </Grid>
-          <button onClick={clearTask}>Clear all Tasks</button>
           </div>
         );
       })
@@ -64,16 +68,15 @@ const Todos = ({ todos }) => {
       <p>{"You have no todo's left"}</p>
     );
 
-    
-
+    <script>todoList = globalList</script>
 
     // Lastly, return the todoList constant that we created above to show all of the items on the screen.
     return (
       <div className="todoCollection" style={{ padding: "10px" }}>
         
-        <script>todoList = globalList</script>
         {todoList}
-        
+        <button onClick={clearTask}>Clear all Tasks</button>
+
       </div>
     );
 };
